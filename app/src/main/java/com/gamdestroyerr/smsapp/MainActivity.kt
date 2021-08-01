5bdd5698c7b34d73a5caf105246b0a3143216b0a
noob-gamedestroyer <60071765+noob-gamedestroyer@users.noreply.github.com>
2021-08-01 23:47:02 +0530

    added notification support

--- app/src/main/java/com/gamdestroyerr/smsapp/MainActivity.kt
+++ app/src/main/java/com/gamdestroyerr/smsapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.gamdestroyerr.smsapp
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.provider.Telephony
 import android.util.Log
@@ -26,6 +30,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        createNotificationChannel()
+
         val permission = ContextCompat.checkSelfPermission(
             this,
             Manifest.permission.RECEIVE_SMS
@@ -35,9 +41,23 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, appPermission, SMS_REQUEST_CODE)
         }
 
-        receiver = SmsBroadcastReceiver(binding)
+        receiver = SmsBroadcastReceiver(binding,this)
         IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION).also {
             registerReceiver(receiver, it)
+        }
+    }
+
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
@@ -47,8 +67,9 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         when (requestCode) {
-            Companion.SMS_REQUEST_CODE -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            SMS_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0]
+                            == PackageManager.PERMISSION_GRANTED)) {
                     Log.d("TAG", "Permission has been Granted by user")
                 } else {
                     Log.d("TAG", "Permission has been denied by user")
@@ -61,12 +82,14 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    override fun onStop() {
-        super.onStop()
-        unregisterReceiver(receiver)
-    }
+//    override fun onStop() {
+//        super.onStop()
+//        unregisterReceiver(receiver)
+//    }
 
     companion object {
         private const val SMS_REQUEST_CODE = 100
+        private const val CHANNEL_NAME = "message"
+        const val CHANNEL_ID = "sms_app_notify"
     }
 }
--- app/src/main/java/com/gamdestroyerr/smsapp/SmsBroadcastReciever.kt
+++ app/src/main/java/com/gamdestroyerr/smsapp/SmsBroadcastReciever.kt
@@ -4,16 +4,38 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
 import com.gamdestroyerr.smsapp.databinding.ActivityMainBinding
 
-class SmsBroadcastReceiver(private var _binding: ActivityMainBinding): BroadcastReceiver() {
-
+class SmsBroadcastReceiver(
+    private var _binding: ActivityMainBinding,
+    private val mainActivityContext: Context
+    ): BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
 
         val message = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        val builder = NotificationCompat.Builder(mainActivityContext, MainActivity.CHANNEL_ID)
+        val notificationManager = NotificationManagerCompat.from(mainActivityContext)
+        val intentMain = Intent(mainActivityContext, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(mainActivityContext).run {
+            addNextIntentWithParentStack(intentMain)
+            getPendingIntent(101, 0)
+        }
 
         message.iterator().forEach{
+
+            val notification = builder.setContentTitle(it.displayOriginatingAddress)
+                .setContentText(it.displayMessageBody)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setSmallIcon(R.drawable.ic_round_message_24)
+                .setContentIntent(pendingIntent)
+                .build()
+
+            notificationManager.notify(0, notification)
+
             _binding.apply {
                 smsPhoneNo.text = it.displayOriginatingAddress
                 smsBody.text = it.displayMessageBody
